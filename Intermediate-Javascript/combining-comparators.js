df3e9094b0cb5072e6d24566c1606e16aff7453d
walwizard <goneisgone@hotmail.com>
2023-03-08 13:45:14 -0300

    130 - Combining Comparators

---
+++ Intermediate-Javascript/combining-comparators.js
@@ -0,0 +1,16 @@
+//Combining Comparators
+
+/*
+    || OR
+    && AND
+    ! NOT
+*/
+
+let n1 = 15;
+let n2 = 15;
+
+if(n1 === 15 && n2 === 15) {
+    console.log("It's Equal");
+} else {
+    console.log("NOT equal");
+}
