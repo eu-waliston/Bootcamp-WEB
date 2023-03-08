9f374f620eb644ccdb2f298f1b2e0d7edd570c5a
walwizard <goneisgone@hotmail.com>
2023-03-08 14:14:15 -0300

    133 - Collections Working with Javascript Arrays

---
+++ Intermediate-Javascript/arrays.js
@@ -0,0 +1,16 @@
+//Collections Working with Javascript Arrays
+let guests = [
+    "Alex",
+    "Angela",
+    "Tom",
+    "Willian",
+    "Lukas",
+    "Maykon"
+]
+let yourName = "Json"
+if(guests.includes(yourName)) {
+    console.log("Your are on the list welcome " + yourName);
+} {
+    console.log("Sorry your name it's not on the list");
+}
+console.log(guests.includes("Alex"));
