a2f2e085a842e20cba2960382d9ce16ad26ce23a
walwizard <goneisgone@hotmail.com>
2023-03-08 14:47:35 -0300

    134 - Adding Elements and Intermediate Array Techniques

--- Intermediate-Javascript/arrays.js
+++ Intermediate-Javascript/arrays.js
@@ -8,9 +8,28 @@ let guests = [
     "Maykon"
 ]
 let yourName = "Json"
-if(guests.includes(yourName)) {
+if (guests.includes(yourName)) {
     console.log("Your are on the list welcome " + yourName);
 } {
     console.log("Sorry your name it's not on the list");
 }
 console.log(guests.includes("Alex"));
+
+//Adding Elements and Intermediate Array Techniques
+//Push, Pop
+var output = [];
+var count = 1;
+
+function fizzBuzz() {
+    if (count % 2 === 0) {
+        output.push("fizz")
+    } else {
+        output.push(count)
+    }
+
+    count++;
+    console.log(output);
+
+}
+
+fizzBuzz();
