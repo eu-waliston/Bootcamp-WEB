442eccd1b2ea7aa103a6a4eb3a3a38c7ed0951c0
walwizard <goneisgone@hotmail.com>
2023-03-08 13:26:31 -0300

    127 - Random Number Generation in Javascript Building a Love Calculator

---
+++ Intermediate-Javascript/random.js
@@ -0,0 +1,9 @@
+//Random Number Generator
+let n = Math.random();
+n = n * 6
+n = Math.floor(n) + 1
+console.log(n);
+
+//Love calculator
+let loveScore = Math.floor(Math.random() * 100);
+console.log("Your love score is: " + loveScore + "%");
