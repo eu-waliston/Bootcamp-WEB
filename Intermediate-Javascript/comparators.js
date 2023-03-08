1127076482766c207ec1bb890a58a9169c39d59f
walwizard <goneisgone@hotmail.com>
2023-03-08 13:40:49 -0300

    129 - Comparators and Equality

---
+++ Intermediate-Javascript/comparators.js
@@ -0,0 +1,20 @@
+//Comparators and Equality
+
+/*
+=== -> is equal to
+!== -> is not equal to 
+>   -> is greater than
+<   -> is lesser than
+>=  -> is greater or equal to
+<=  -> is lesser or  equal to 
+*/
+
+let n1 = 19;
+let n2 = 2;
+
+console.log(n1 === n2);
+console.log(n1 !== n2);
+console.log(n1 > n2);
+console.log(n1 < n2);
+console.log(n1 >= n2);
+console.log(n1 <= n2);
