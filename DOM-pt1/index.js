2859555fab4cb26cfd1a51f45e6fb014008537e3
walwizard <goneisgone@hotmail.com>
2023-03-10 16:50:58 -0300

    145 - Selecting HTML Elements with Javascript

--- DOM-pt1/index.js
+++ DOM-pt1/index.js
@@ -1,3 +1,7 @@
 document.querySelector("input").click();
 
 let name = document.getElementsByClassName('list')[2].innerHTML = "Waliston";
+
+let body = document.querySelector("body").style.backgroundColor = "red"
+
+let li = document.querySelector("li a").style.color = "white"
