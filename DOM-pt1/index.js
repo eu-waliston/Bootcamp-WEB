564d474efbad3b96c481612449a5bffb0cb43c68
walwizard <goneisgone@hotmail.com>
2023-03-10 17:40:56 -0300

    147 - The Separation of Concerns Structure vs Style vs Behaviour

--- DOM-pt1/index.js
+++ DOM-pt1/index.js
@@ -2,16 +2,24 @@ document.querySelector("input").click();
 
 document.getElementsByClassName('list')[2].innerHTML = "Waliston";
 
-let body = document.querySelector("body").style.backgroundColor = "red"
+
 
 document.querySelector("li a").style.color = "white"
 
 document.querySelector("h1").style.color = "white"
 document.querySelector("h1").style.fontFamily = "sans-serif"
 document.querySelector("h1").style.textAlign = "center"
-document.querySelector("h1").style.fontSize = "50px"
+
 
 document.querySelector("button").style.backgroundColor = "blue"
+
+let btn = document.querySelector("button").addEventListener('click', () => {
+    let body = document.querySelector("body")
+    let h1 = document.querySelector("h1")
+
+    body.classList.toggle('colors')
+    h1.classList.toggle("huge")
+})
 
 
 document.querySelector("button").style.color = "#fff"
