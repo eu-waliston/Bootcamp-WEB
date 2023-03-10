6eb0aadc876eb5fb72a8b250d19d575ae3ba6f07
walwizard <goneisgone@hotmail.com>
2023-03-10 15:23:04 -0300

    Challenge 3

--- Intermediate-Javascript/challenges/challenge-3.js
+++ Intermediate-Javascript/challenges/challenge-3.js
@@ -4,10 +4,13 @@ function fibonacciGenerator(n) {
     let fibbo = [];
     let i;
     for (i = 0; i < n; i++) {
+        if(i === 0) {
+            fibbo.push(0)
+        }
         fibbo.push(i + 1)
     }
 
     return console.log("fibonacci => ["  + fibbo + "]");
 }
 
-fibonacciGenerator(5)
+fibonacciGenerator(15)
