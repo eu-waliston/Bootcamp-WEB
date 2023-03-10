5c29e65350f4bc00ad5cf4eb1446d15330fe38e9
walwizard <goneisgone@hotmail.com>
2023-03-10 17:12:29 -0300

    146 - Manipulating and Changing Styles of HTML Elements with Javascript

--- DOM-pt1/index.js
+++ DOM-pt1/index.js
@@ -1,7 +1,24 @@
 document.querySelector("input").click();
 
-let name = document.getElementsByClassName('list')[2].innerHTML = "Waliston";
+document.getElementsByClassName('list')[2].innerHTML = "Waliston";
 
 let body = document.querySelector("body").style.backgroundColor = "red"
 
-let li = document.querySelector("li a").style.color = "white"
+document.querySelector("li a").style.color = "white"
+
+document.querySelector("h1").style.color = "white"
+document.querySelector("h1").style.fontFamily = "sans-serif"
+document.querySelector("h1").style.textAlign = "center"
+document.querySelector("h1").style.fontSize = "50px"
+
+document.querySelector("button").style.backgroundColor = "blue"
+
+
+document.querySelector("button").style.color = "#fff"
+document.querySelector("button").style.padding = "15px"
+document.querySelector("button").style.borderRadius = "5px"
+
+
+
+
+
