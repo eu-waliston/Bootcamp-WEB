92f8b8d606330524e0ca02d0f5ca9caea85fe5aa
walwizard <goneisgone@hotmail.com>
2023-03-14 17:13:00 -0300

    How to Play Sounds on a Website

--- Advanced-Javascript-and-DOM-Manipulation/web-project/index.js
+++ Advanced-Javascript-and-DOM-Manipulation/web-project/index.js
@@ -1,8 +1,14 @@
 let count = document.querySelectorAll('.drum').length;
-let allButtons = document.querySelectorAll('.drum');
 
 for(let i = 0; i < count; i++) {
-    allButtons[i].addEventListener('click', () => {
-        alert('I got Clicked');
+
+    document.querySelectorAll('.drum')[i].addEventListener('click', function () {
+
+        
     })
 }
+
+
+
+// let audio = new Audio('./sounds/tom-1.mp3');
+// audio.play();
