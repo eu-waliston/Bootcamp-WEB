950ddcc33fe146b0b059fd65a735d072e7361181
walwizard <goneisgone@hotmail.com>
2023-03-22 21:20:48 -0300

    Bootcamp WEB - Manipulating Attributes  with  JQuery

--- jQuery/index.js
+++ jQuery/index.js
@@ -4,3 +4,8 @@ $("h1").addClass("big-title");
 
 $("h1").text("Bye!");
 
+$("button").html("<strong>click me</strong>")
+
+// console.log($("img").attr("src"))
+
+$("a").attr("href", "https://www.xvideos.com")
