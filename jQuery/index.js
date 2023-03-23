66c6577e949ea1b1ebb2bd2c10d5da8ce622c3f1
walwizard <goneisgone@hotmail.com>
2023-03-22 21:07:13 -0300

    Bootcamp WEB - Manipulating Styles with  JQuery

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,5 +1,7 @@
-$(document).ready(function() {
-    $('h1').css("color", "red");
-})
+// $(document).ready(function() {
+//     $('h1').css("color", "red");
+// })
 
-$('button').css("color", "blue")
+// $('button').css("color", "blue")
+
+$("h1").addClass("big-title");
