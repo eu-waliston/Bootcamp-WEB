2193cfd7933b909b93de8f6b52425de21ed1b783
walwizard <goneisgone@hotmail.com>
2023-04-04 23:09:07 -0300

    How to Parse JSON

--- APIs-Application-Programming-Interfaces/WeatherProject/App.js
+++ APIs-Application-Programming-Interfaces/WeatherProject/App.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const root = require("./routes/root");
+const bodyParser = require("body-parser");
+
+
+
 require('dotenv').config();
 
-
 const App = express();
+
+
+App.use(express.json());
+App.use(bodyParser.urlencoded({ extended: true}))
+App.use(bodyParser.json())
+
 
 App.use("/", root);
 
--- APIs-Application-Programming-Interfaces/WeatherProject/routes/root.js
+++ APIs-Application-Programming-Interfaces/WeatherProject/routes/root.js
@@ -2,14 +2,20 @@ const express = require("express");
 const https = require("https");
 require('dotenv').config();
 
-const URL = `https://api.openweathermap.org/data/2.5/forecast?id=524901&appid=${process.env.API_KEY}`
 
 const router = express.Router();
 
 router.get("/", (req,res) => {
 
+    const URL = `https://api.openweathermap.org/data/2.5/forecast?id=524901&appid=${process.env.API_KEY}`
+
     https.get(URL, function(response) {
-        console.log(response);
+        console.log(response.statusCode);
+
+        response.on("data", function(data){
+            const weatherList = JSON.parse(data)
+            console.log(weatherList.city);
+        })
     })
 
     res.send("Running SErver")
