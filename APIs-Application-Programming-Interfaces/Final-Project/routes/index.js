472f16cf213b17ace6bcf939ea8e6bfd2ad2ff3f
walwizard <goneisgone@hotmail.com>
2023-04-06 21:51:58 -0300

    Final Module Project - Posting Data t oMailchimp Server via their API

--- APIs-Application-Programming-Interfaces/Final-Project/routes/index.js
+++ APIs-Application-Programming-Interfaces/Final-Project/routes/index.js
@@ -1,14 +1,50 @@
 const express = require("express");
 const router = express.Router();
-const path = require("path")
+const path = require("path");
+const https = require("https");
+require('dotenv').config()
 
 router.get("/", (req, res) => {
   res.status(200).sendFile(path.join(__dirname, "..", "public", "signup.html"));
 });
 
-router.post("/", (req,res) => {
-  let data = req.body;
-  console.log(data)
-})
+router.post("/", (req, res) => {
+
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+
+  let data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        },
+      },
+    ],
+  };
+
+  let jsonDate = JSON.stringify(data);
+
+  const url = `https://us21.api.mailchimp.com/3.0/lists/${process.env.LIST_ID}`
+
+  const option = {
+    method: "POST",
+    auth: `walwizard:${process.env.API_KEY}`
+  }
+
+  const request = https.request(url, option, function(response) {
+    response.on("data", function(data) {
+      console.log(JSON.parse(data));
+    })
+  })
+
+
+  request.write(jsonDate);
+  request.end();
+});
 
 module.exports = router;
