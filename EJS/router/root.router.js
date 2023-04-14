3a762a07a3673a81878dee86528ee8c35dc146e2
walwizard <goneisgone@hotmail.com>
2023-04-13 22:05:43 -0300

    Understanding templating vs layout

--- EJS/router/root.router.js
+++ EJS/router/root.router.js
@@ -2,26 +2,43 @@ const express = require("express");
 const router = express.Router();
 
 let tasks = ["ir ao mercado", "cortar o cabelo"];
+let workItems = [];
 
 router.get("/", (req, res) => {
-
   let today = new Date();
 
   let options = {
     weekday: "long",
     day: "numeric",
     month: "long",
-  }
+  };
 
-  let day = today.toLocaleDateString("pt-BR", options)
+  let day = today.toLocaleDateString("pt-BR", options);
 
-  res.render("list", { kindOfday: day, newListItems: tasks });
+  res.render("list", { listTitle: day, newListItems: tasks });
 });
 
 router.post("/", (req, res) => {
-  tasks.push(req.body.newItem)
+  let item = req.body.newItem;
 
-  res.redirect("/")
-})
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    tasks.push(item);
+    res.redirect("/");
+  }
+});
+
+router.get("/work", (req, res) => {
+  res.render("list", { listTitle: "Work List", newListItems: workItems });
+});
+
+router.post("/work", (req, res) => {
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+});
+
 //271
 module.exports = router;
