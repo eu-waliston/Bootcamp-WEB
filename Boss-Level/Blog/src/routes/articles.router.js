b6ab01dd1d0e3e22f139aac5235f6153ccfe735f
walwizard <goneisgone@hotmail.com>
2023-04-18 22:26:38 -0300

    Boo Leve - Blog Pages

--- Boss-Level/Blog/src/routes/articles.router.js
+++ Boss-Level/Blog/src/routes/articles.router.js
@@ -2,8 +2,16 @@ const express = require("express");
 const articleRouter = express.Router();
 const path = require("path");
 
-articleRouter.get('/', (req,res) => {
+articleRouter.get('/home', (req,res) => {
     res.render(path.join(__dirname, "../views", "home.ejs"));
+})
+
+articleRouter.get('/about', (req,res) => {
+    res.render(path.join(__dirname, "../views", "about.ejs"))
+})
+
+articleRouter.get('/post', (req,res) => {
+    res.render(path.join(__dirname, "../views", "post.ejs"))
 })
 
 module.exports = articleRouter;
