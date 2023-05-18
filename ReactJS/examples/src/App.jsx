41bf41d75f7e75136193e94b0d1490b0f3b26125
walwizard <goneisgone@hotmail.com>
2023-05-18 20:39:15 -0300

    React Hooks useStatec

--- ReactJS/examples/src/App.jsx
+++ ReactJS/examples/src/App.jsx
@@ -1,20 +1,36 @@
-import React from "react"
+import React, { useState} from "react"
 import "./App.css"
+
 
 const App = () => {
   // let isDone = false;
   // const strikeThrough = { textDecoration: "line-through"}
-  function strike() {
-    document.getElementById("root").style.textDecoration = "line-through"
+  // function strike() {
+  //   document.getElementById("root").style.textDecoration = "line-through"
+  // }
+
+  let [count, setCount] = useState(0);
+  function increase() {
+    setCount(count + 1)
+  }
+
+  const decrease = () => {
+    setCount(count - 1)
   }
 
   return (
     <div className="App">
-      <h1>Buy Milk</h1>
+      {/* <h1>Buy Milk</h1> */}
       {/* <h1 style={  isDone ? strikeThrough : null }>Buy Milk</h1> */}
-      <button onClick={strike}>
+      {/* <button onClick={strike}>
         change to strike through
-      </button>
+      </button> */}
+
+      {/* ------------------ */}
+
+      <h1>{count}</h1>
+      <button onClick={increase}>+</button>
+      <button onClick={decrease}>-</button>
     </div>
   )
 }
