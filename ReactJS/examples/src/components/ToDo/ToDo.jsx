2e283740bf90d26c986818231ae0f7b9df9833b1
walwizard <goneisgone@hotmail.com>
2023-05-29 20:31:38 -0300

    Bootcamp WEB - Managing a Component Tree

--- ReactJS/examples/src/components/ToDo/ToDo.jsx
+++ ReactJS/examples/src/components/ToDo/ToDo.jsx
@@ -1,5 +1,7 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./ToDo.css";
+import ToDiIten from "./ToDoIten";
+
 
 const TODO = () => {
 
@@ -18,21 +20,43 @@ const TODO = () => {
         setInputText("");
     }
 
+    function deleteIten(id){
+        setItems((prevItens) => {
+            return prevItens.filter(
+                (item, index ) => {
+                    return index !== id
+                }
+            )
+        })
+    }
+
     return (
         <div className="container">
             <div className="heading">
                 <h1>To-Do List</h1>
             </div>
             <div className="form">
-                <input onChange={handleChange} type="text" value={inputText} />
+                <input
+                    onChange={handleChange}
+                    type="text"
+                    value={inputText}
+                />
                 <button onClick={addItem}>
                     <span>Add</span>
+                    
                 </button>
             </div>
             <div>
                 <ul>
-                    {items.map(todoItem => (
-                        <li>{todoItem}</li>
+                    {items.map((todoItem, index) => (
+
+                        <ToDiIten
+                            key={index}
+                            id={index}
+                            title={todoItem}
+                            onChecked={deleteIten}
+                        />
+
                     ))}
                 </ul>
             </div>
---
+++ ReactJS/examples/src/components/ToDo/ToDoIten.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+
+const ToDiIten = (props) => {
+
+    const [done, setDone] = useState(true);
+
+    const setDoneStyle = () => {
+        setDone(!done)
+    }
+
+    return (
+        <div className="todo--iten">
+            <i className="bi bi-check" onClick={setDoneStyle}></i>
+
+            <li className={done ? "iten" : "iten--effect"}>
+                {props.title}
+            </li>
+
+            <i className="bi bi-trash2" onClick={() => {
+                props.onChecked(props.id);
+            }}></i>
+        </div>
+    )
+}
+
+export default ToDiIten;
